4e39f315db451f8613703bed49db31e5031298a8
Joshua Chi <joshokn@gmail.com>
2025-03-24 10:36:42 +0800

    refs #11: add unit tests for handle_client_proposal_in_batch

--- src/core/replication/replication_handler_test.rs
+++ src/core/replication/replication_handler_test.rs
@@ -1,13 +1,279 @@
 use super::{ReplicationCore, ReplicationHandler};
 use crate::{
+    grpc::rpc_service::Entry,
     test_utils::{
-        setup_raft_components, MockNode, MockTypeConfig, MOCK_REPLICATION_HANDLER_PORT_BASE,
+        setup_raft_components, simulate_insert_proposal, MockNode, MockTypeConfig,
+        MOCK_REPLICATION_HANDLER_PORT_BASE,
     },
     AppendResults, ChannelWithAddressAndRole, Error, LeaderStateSnapshot, MockRaftLog,
-    MockTransport, PeerUpdate, StateSnapshot, FOLLOWER,
+    MockTransport, PeerUpdate, RaftLog, RaftTypeConfig, StateSnapshot, FOLLOWER,
 };
 use std::{collections::HashMap, sync::Arc};
 use tokio::sync::oneshot;
+
+/// # Case 1: The peer3's next_index is equal to
+///     the end of the leader's old log,
+///     and only the new log is sent
+///
+/// ## Validate criterias
+/// 1. only new_entries returned
+///
+#[test]
+fn test_retrieve_to_be_synced_logs_for_peers_case1() {
+    let context = setup_raft_components(
+        "/tmp/test_retrieve_to_be_synced_logs_for_peers_case1",
+        None,
+        false,
+    );
+    let my_id = 1;
+    let peer3_id = 3;
+    let new_entries = vec![Entry {
+        index: 1,
+        term: 1,
+        command: vec![1; 8],
+    }];
+    let leader_last_index_before_inserting_new_entries = 10;
+    let max_entries = 100;
+    let peer_next_indices =
+        HashMap::from([(peer3_id, leader_last_index_before_inserting_new_entries)]);
+    let handler = ReplicationHandler::<RaftTypeConfig>::new(my_id);
+
+    let r = handler.retrieve_to_be_synced_logs_for_peers(
+        new_entries.clone(),
+        leader_last_index_before_inserting_new_entries,
+        max_entries,
+        &peer_next_indices,
+        &context.raft_log,
+    );
+    if let Some(entries) = r.get(&peer3_id) {
+        assert_eq!(*entries, new_entries, "Entries do not match expected value");
+    } else {
+        assert!(false);
+    };
+}
+
+/// # Case 2: Peer3 needs old log + new log
+///     (not exceeding the max limit)
+///     and returning latencies and the new log
+///
+/// ## Prepration setup
+/// 1. Simulate one entry in local raft log(log-1)
+/// 2. Peer 3 next_index is 1
+///
+/// ## Validate criterias
+/// 1. both log-1 and new_entries are returned
+///
+#[test]
+fn test_retrieve_to_be_synced_logs_for_peers_case2() {
+    let context = setup_raft_components(
+        "/tmp/test_retrieve_to_be_synced_logs_for_peers_case2",
+        None,
+        false,
+    );
+
+    // Simulate one entry in local raft log
+    let raft_log = context.raft_log;
+    simulate_insert_proposal(&raft_log, vec![1], 1);
+
+    let my_id = 1;
+    let peer3_id = 3;
+    let new_entries = vec![Entry {
+        index: 2,
+        term: 1,
+        command: vec![1; 8],
+    }];
+    let leader_last_index_before_inserting_new_entries = 1;
+    let max_entries = 100;
+    let peer_next_indices =
+        HashMap::from([(peer3_id, leader_last_index_before_inserting_new_entries)]);
+    let handler = ReplicationHandler::<RaftTypeConfig>::new(my_id);
+
+    let r = handler.retrieve_to_be_synced_logs_for_peers(
+        new_entries.clone(),
+        leader_last_index_before_inserting_new_entries,
+        max_entries,
+        &peer_next_indices,
+        &raft_log,
+    );
+    let last_log_entry = raft_log.last().unwrap();
+    let mut merged_entries = vec![last_log_entry];
+    merged_entries.extend(new_entries);
+    if let Some(entries) = r.get(&peer3_id) {
+        assert_eq!(
+            *entries, merged_entries,
+            "Entries do not match expected value"
+        );
+    } else {
+        assert!(false);
+    };
+}
+
+/// # Case 3: new_entries is empty and Peer3 has latencies
+///     (not exceeding the max limit)
+///
+/// ## Prepration setup
+/// 1. Simulate one entry in local raft log(log-1)
+/// 2. Peer 3 next_index is 1
+///
+/// ## Validate criterias
+/// 1. only log-1 is returned
+///
+#[test]
+fn test_retrieve_to_be_synced_logs_for_peers_case3() {
+    let context = setup_raft_components(
+        "/tmp/test_retrieve_to_be_synced_logs_for_peers_case3",
+        None,
+        false,
+    );
+
+    // Simulate one entry in local raft log
+    let raft_log = context.raft_log;
+    simulate_insert_proposal(&raft_log, vec![1], 1);
+
+    let my_id = 1;
+    let peer3_id = 3;
+    let new_entries = vec![];
+    let leader_last_index_before_inserting_new_entries = 1;
+    let max_entries = 100;
+    let peer_next_indices =
+        HashMap::from([(peer3_id, leader_last_index_before_inserting_new_entries)]);
+    let handler = ReplicationHandler::<RaftTypeConfig>::new(my_id);
+
+    let r = handler.retrieve_to_be_synced_logs_for_peers(
+        new_entries.clone(),
+        leader_last_index_before_inserting_new_entries,
+        max_entries,
+        &peer_next_indices,
+        &raft_log,
+    );
+    let last_log_entry = raft_log.last().unwrap();
+    if let Some(entries) = r.get(&peer3_id) {
+        assert_eq!(
+            *entries,
+            vec![last_log_entry],
+            "Entries do not match expected value"
+        );
+    } else {
+        assert!(false);
+    };
+}
+
+/// # Case 4.1: Peer3 needs old log + new log - max_legacy_entries_per_peer = 2
+///     (while exceeding the max limit)
+///     and returning latencies and the new log
+///
+/// ## Prepration setup
+/// 1. Simulate entries in local raft log(log-1, log-2, log-3)
+/// 2. Peer 3 next_index is 1
+/// 3. max_legacy_entries_per_peer = 2
+///
+/// ## Validate criterias
+/// 1. both log-1,log-2 and new_entries are returned
+///
+#[test]
+fn test_retrieve_to_be_synced_logs_for_peers_case4_1() {
+    let context = setup_raft_components(
+        "/tmp/test_retrieve_to_be_synced_logs_for_peers_case4_1",
+        None,
+        false,
+    );
+
+    let my_id = 1;
+    let peer3_id = 3;
+    let peer3_next_id = 1;
+    // Simulate one entry in local raft log
+    let max_legacy_entries_per_peer = 2;
+    let leader_last_index_before_inserting_new_entries = 3;
+    let raft_log = context.raft_log;
+    simulate_insert_proposal(
+        &raft_log,
+        (1..=leader_last_index_before_inserting_new_entries).collect(),
+        1,
+    );
+
+    let new_entries = vec![Entry {
+        index: 3,
+        term: 1,
+        command: vec![1; 8],
+    }];
+    let peer_next_indices = HashMap::from([(peer3_id, peer3_next_id)]);
+    let handler = ReplicationHandler::<RaftTypeConfig>::new(my_id);
+
+    let r = handler.retrieve_to_be_synced_logs_for_peers(
+        new_entries.clone(),
+        leader_last_index_before_inserting_new_entries,
+        max_legacy_entries_per_peer,
+        &peer_next_indices,
+        &raft_log,
+    );
+    let mut lagency_entries = raft_log
+        .get_entries_between(peer3_next_id..=(peer3_next_id + max_legacy_entries_per_peer - 1));
+    lagency_entries.extend(new_entries);
+    if let Some(entries) = r.get(&peer3_id) {
+        assert_eq!(
+            *entries, lagency_entries,
+            "Entries do not match expected value"
+        );
+    } else {
+        assert!(false);
+    };
+}
+
+/// # Case 4.2: Peer3 needs old log + new log - max_legacy_entries_per_peer = 0
+///     (while exceeding the max limit)
+///     and returning latencies and the new log
+///
+/// ## Prepration setup
+/// 1. Simulate entriesin local raft log(log-1, log-2, log-3)
+/// 2. Peer 3 next_index is 1
+/// 3. max_legacy_entries_per_peer = 0
+///
+/// ## Validate criterias
+/// 1. only new_entries are returned
+///
+#[test]
+fn test_retrieve_to_be_synced_logs_for_peers_case4_2() {
+    let context = setup_raft_components(
+        "/tmp/test_retrieve_to_be_synced_logs_for_peers_case4_2",
+        None,
+        false,
+    );
+
+    let my_id = 1;
+    let peer3_id = 3;
+    let peer3_next_id = 1;
+    // Simulate one entry in local raft log
+    let max_legacy_entries_per_peer = 0;
+    let leader_last_index_before_inserting_new_entries = 3;
+    let raft_log = context.raft_log;
+    simulate_insert_proposal(
+        &raft_log,
+        (1..=leader_last_index_before_inserting_new_entries).collect(),
+        1,
+    );
+
+    let new_entries = vec![Entry {
+        index: 3,
+        term: 1,
+        command: vec![1; 8],
+    }];
+    let peer_next_indices = HashMap::from([(peer3_id, peer3_next_id)]);
+    let handler = ReplicationHandler::<RaftTypeConfig>::new(my_id);
+
+    let r = handler.retrieve_to_be_synced_logs_for_peers(
+        new_entries.clone(),
+        leader_last_index_before_inserting_new_entries,
+        max_legacy_entries_per_peer,
+        &peer_next_indices,
+        &raft_log,
+    );
+
+    if let Some(entries) = r.get(&peer3_id) {
+        assert_eq!(*entries, new_entries, "Entries do not match expected value");
+    } else {
+        assert!(false);
+    };
+}
 
 /// # Case 1: No peers found
 /// ## Validation Criteria
@@ -85,7 +351,7 @@ async fn test_handle_client_proposal_in_batch_case1() {
 #[tokio::test]
 async fn test_handle_client_proposal_in_batch_case2_1() {
     let context = setup_raft_components(
-        "/tmp/test_handle_client_proposal_in_batch_case2",
+        "/tmp/test_handle_client_proposal_in_batch_case2_1",
         None,
         false,
     );
@@ -139,6 +405,7 @@ async fn test_handle_client_proposal_in_batch_case2_1() {
         .returning(|| 1);
     raft_log.expect_get_entries_between().returning(|_| vec![]);
     raft_log.expect_prev_log_term().returning(|_, _| 0);
+
     let mut transport = MockTransport::new();
     transport
         .expect_send_append_requests()
@@ -158,7 +425,6 @@ async fn test_handle_client_proposal_in_batch_case2_1() {
         .await
     {
         assert_eq!(append_result.commit_quorum_achieved, true);
-        println!("append_result: {:?}", &append_result);
     } else {
         assert!(false);
     }
