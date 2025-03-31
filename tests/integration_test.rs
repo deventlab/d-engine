482588c459d5640f02aa8627f48448bfc7ebba09
Joshua Chi <joshokn@gmail.com>
2025-03-31 18:22:53 +0800

    fix integration log init bug

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,3 +1,12 @@
 mod append_entries;
 mod cluster_start_stop;
 mod commons;
+
+static LOGGER_INIT: once_cell::sync::Lazy<()> = once_cell::sync::Lazy::new(|| {
+    env_logger::init();
+});
+
+pub fn enable_logger() {
+    *LOGGER_INIT;
+    println!("setup logger for unit test.");
+}
